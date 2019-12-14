b3527522f0ec9d8de1b0178268b23f436858f7d6
kasulejoseph <kasule06joseph@gmail.com>
2019-12-14 18:37:08 +0300

    add status chips

--- cashcog/src/components/ExpenseCard.vue
+++ cashcog/src/components/ExpenseCard.vue
@@ -9,7 +9,17 @@
         <v-list-item-subtitle v-text="employee"></v-list-item-subtitle>
         <v-list-item-title style="color:#FEC42E" class="headline mb-1"  v-text="currency"></v-list-item-title>
         <div class="subtitle-1 font-weight-regular" v-text="description"></div>
-            <v-switch v-model="switch1" inset :label="`approved: ${switch1.toString()}`"></v-switch>
+        <span class="mb-2 mt-3"></span>
+        <v-chip-group
+        v-model="amenities"
+        column
+        mandatory
+      >
+        <v-chip filter outlined>Pending</v-chip>
+        <v-chip filter outlined>Aprroved</v-chip>
+        <v-chip filter outlined>Rejected</v-chip>
+      </v-chip-group>
+            <!-- <v-switch v-model="switch1" inset :label="`approved: ${switch1.toString()}`"></v-switch> -->
       </v-list-item-content>
 
     </v-list-item>
